41df08ada6adc3755b9054a5cba5494db3530150
liber09 <linda.bergsangel@hotmail.com>
2022-12-12 04:43:43 +0100

    Added first data post

--- app/src/main/java/com/example/axezziblerestaurants/MainActivity.kt
+++ app/src/main/java/com/example/axezziblerestaurants/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.example.axezziblerestaurants
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+//initialize database
+val db = Firebase.firestore
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val db = Firebase.firestore
+    }
+
+    fun CreateUser(){
+        //Create a user with firstname and lastname
+        val user = hashMapOf(
+            "first" to "Linda",
+            "last" to "Bergsängel"
+            )
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
