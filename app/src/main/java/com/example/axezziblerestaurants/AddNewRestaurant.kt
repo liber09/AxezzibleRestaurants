48670948eda41f9ae4a26a872c11fd099c016ef3
liber09 <linda.bergsangel@hotmail.com>
2022-12-13 04:39:15 +0100

    New activity for adding restaurants

---
+++ app/src/main/java/com/example/axezziblerestaurants/AddNewRestaurant.kt
@@ -0,0 +1,11 @@
+package com.example.axezziblerestaurants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewRestaurant : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_restaurant)
+    }
+}
