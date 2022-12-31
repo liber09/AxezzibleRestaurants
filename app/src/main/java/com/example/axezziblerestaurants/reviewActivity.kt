8052bd6c18c7cf6eaa96fb997cdbd554c063eb9a
liber09 <linda.bergsangel@hotmail.com>
2022-12-31 07:12:00 +0100

    go back button working

--- app/src/main/java/com/example/axezziblerestaurants/reviewActivity.kt
+++ app/src/main/java/com/example/axezziblerestaurants/reviewActivity.kt
@@ -2,6 +2,7 @@ package com.example.axezziblerestaurants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.RatingBar
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -12,6 +13,7 @@ class reviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+        //Get data from the other activity
         val intent = intent
         val restaurantPosition = intent.getIntExtra(RESTAURANT_POSITION_KEY, POSITION_NOT_SET) //Get the restaurant position from calling activity
         restaurant = DataManager.restaurants[restaurantPosition]
@@ -19,5 +21,10 @@ class reviewActivity : AppCompatActivity() {
         reviewTextView.text = restaurant.review
         val reviewBarView = findViewById<RatingBar>(R.id.reviewRatingBar)
         reviewBarView.rating = restaurant.rating.toFloat()
+        val returnButton = findViewById<Button>(R.id.goBackButton)
+        returnButton.setOnClickListener{
+            finish()
+        }
+
     }
 }
