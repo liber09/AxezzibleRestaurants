ba5838d5b51e888d6a4241d24d20452c795b21e0
liber09 <linda.bergsangel@hotmail.com>
2023-01-06 03:37:11 +0100

    createed a new review

---
+++ app/src/main/java/com/example/axezziblerestaurants/Review.kt
@@ -0,0 +1,7 @@
+package com.example.axezziblerestaurants
+
+class Review (
+    val name: String = "",
+    val reviewText: String = "",
+    val rating: Double = 0.0,
+    val restaurantId: String)
